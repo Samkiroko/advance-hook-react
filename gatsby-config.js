3e11776ad3bcadcb8e4ef70af2131e864073447a
Samkiroko <kirokodev@gmail.com>
2021-09-06 15:07:14 +0300

    correcting section path

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,11 +4,9 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: `lytx code `,
-    description: `Don’t skip design. Learn design and code, by building
-									real apps with React and Swift. Complete courses about
-									the best tools`,
-    author: `@kirokonjenga`,
+    title: `Design+Code`,
+    description: `Don't skip design. Learn design and code, by building real apps with React and Swift. Compete courses about the bbest tools.`,
+    author: `@DesignCode`,
     keywords: `react, hooks, courses, tutorials, web, ios, development`,
     image:
       "https://images.ctfassets.net/ooa29xqb8tix/24XYZktClnZjDjt2YNlGmX/d857becac30265e19ad8847365c68fbf/Metadata.png",
@@ -25,7 +23,7 @@ module.exports = {
       },
     },
     {
-      resolve: "gatsby-source-contentful",
+      resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.GATSBY_CONTENTFUL_SPACEID,
         accessToken: process.env.GATSBY_CONTENTFUL_DELIVER_TOKEN,
